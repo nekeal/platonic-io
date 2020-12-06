bd9eacfc51aa9dff17d2757258245b5aafa20c2d
Szymon Cader <szymon.sc.cader@gmail.com>
2020-12-06 19:30:23 +0100

    Reformat code

--- platonic_io/get_plate.py
+++ platonic_io/get_plate.py
@@ -1,23 +1,19 @@
+from os.path import splitext
+
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import gridspec
+from keras.models import model_from_json
 
 from .tutek_local_utils import detect_lp
-from os.path import splitext, basename
-from keras.models import model_from_json
-import glob
-import time
-from sklearn.preprocessing import LabelEncoder
 
 
 def load_model(path):
     try:
         path = splitext(path)[0]
-        with open('%s.json' % path, 'r') as json_file:
+        with open("%s.json" % path, "r") as json_file:
             model_json = json_file.read()
         model = model_from_json(model_json, custom_objects={})
-        model.load_weights('%s.h5' % path)
+        model.load_weights("%s.h5" % path)
         print("Loading model successfully...")
         return model
     except Exception as e:
@@ -62,13 +58,17 @@ def draw_box(image_path, cor, thickness=3):
 def sort_contours(cnts, reverse=False):
     i = 0
     boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
+    (cnts, boundingBoxes) = zip(
+        *sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse)
+    )
     return cnts
 
+
 # pre-processing input images and pedict with model
-def predict_from_model(image,model,labels):
-    image = cv2.resize(image,(80,80))
-    image = np.stack((image,)*3, axis=-1)
-    prediction = labels.inverse_transform([np.argmax(model.predict(image[np.newaxis,:]))])
+def predict_from_model(image, model, labels):
+    image = cv2.resize(image, (80, 80))
+    image = np.stack((image,) * 3, axis=-1)
+    prediction = labels.inverse_transform(
+        [np.argmax(model.predict(image[np.newaxis, :]))]
+    )
     return prediction
--- platonic_io/recognition_engine.py
+++ platonic_io/recognition_engine.py
@@ -3,10 +3,11 @@ from threading import Thread
 from time import sleep
 
 import cv2
+import numpy as np
 from keras.models import model_from_json
 from sklearn.preprocessing import LabelEncoder
-from .get_plate import load_model, preprocess_image, get_plate, draw_box, sort_contours, predict_from_model
-import numpy as np
+
+from .get_plate import get_plate, load_model, predict_from_model, sort_contours
 
 
 class FrameWorker(Process):
@@ -19,7 +20,7 @@ class FrameWorker(Process):
 
         # Load Models
         # Load model architecture, weight and labels
-        json_file = open('models/MobileNets_character_recognition.json', 'r')
+        json_file = open("models/MobileNets_character_recognition.json", "r")
         loaded_model_json = json_file.read()
         json_file.close()
         model = model_from_json(loaded_model_json)
@@ -29,7 +30,7 @@ class FrameWorker(Process):
         print("[INFO]{} Model loaded successfully...".format(self.name))
 
         labels = LabelEncoder()
-        labels.classes_ = np.load('models/license_character_classes.npy')
+        labels.classes_ = np.load("models/license_character_classes.npy")
         print("[INFO]{} Labels loaded successfully...".format(self.name))
 
         while True:
@@ -37,7 +38,7 @@ class FrameWorker(Process):
             print("{} processing frame {}".format(self.name, frame_idx))
             if frame is None:
                 break
-            LpImg, cor = get_plate(frame,wpod_net)
+            LpImg, cor = get_plate(frame, wpod_net)
 
             if LpImg and (len(LpImg)):  # check if there is at least one license image
                 print("Detect {} plate(s), process {}".format(len(LpImg), self.name))
@@ -48,15 +49,19 @@ class FrameWorker(Process):
                 blur = cv2.GaussianBlur(gray, (7, 7), 0)
 
                 # Applied inversed thresh_binary
-                binary = cv2.threshold(blur, 180, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-                ## Applied dilation
+                binary = cv2.threshold(
+                    blur, 180, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+                )[1]
+                # Applied dilation
                 kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
                 thre_mor = cv2.morphologyEx(binary, cv2.MORPH_DILATE, kernel3)
 
                 # creat a copy version "test_roi" of plat_image to draw bounding box
                 test_roi = plate_image.copy()
                 # contours
-                cont, _ = cv2.findContours(thre_mor, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                cont, _ = cv2.findContours(
+                    thre_mor, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+                )
                 crop_characters = []
 
                 # define standard width and height of character
@@ -66,20 +71,29 @@ class FrameWorker(Process):
                     (x, y, w, h) = cv2.boundingRect(c)
                     ratio = h / w
                     if 1 <= ratio <= 3.5:  # Only select contour with defined ratio
-                        if h / plate_image.shape[0] >= 0.3:  # Select contour which has the height larger than 50% of the plate
+                        if (
+                            h / plate_image.shape[0] >= 0.3
+                        ):  # Select contour which has the height
+                            # larger than 50% of the plate
                             # Draw bounding box arroung digit number
-                            cv2.rectangle(test_roi, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                            cv2.rectangle(
+                                test_roi, (x, y), (x + w, y + h), (0, 255, 0), 2
+                            )
 
                             # Sperate number and gibe prediction
-                            curr_num = thre_mor[y:y + h, x:x + w]
+                            curr_num = thre_mor[y : y + h, x : x + w]
                             curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
-                            _, curr_num = cv2.threshold(curr_num, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                            _, curr_num = cv2.threshold(
+                                curr_num, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+                            )
                             crop_characters.append(curr_num)
 
                 print("{} Detect {} letters...".format(self.name, len(crop_characters)))
-                final_string = ''
+                final_string = ""
                 for i, character in enumerate(crop_characters):
-                    title = np.array2string(predict_from_model(character, model, labels))
+                    title = np.array2string(
+                        predict_from_model(character, model, labels)
+                    )
                     final_string += title.strip("'[]")
                 print("{} Achieved result: ".format(self.name), final_string)
 
@@ -87,6 +101,7 @@ class FrameWorker(Process):
             else:
                 # no plate detected
                 self.result_queue.put((frame_idx, ["", frame]))
+
 
 class Master(Thread):
     def __init__(self, input_path, output_path, worker_count=1):
@@ -97,9 +112,19 @@ class Master(Thread):
 
     def run(self):
         movie = cv2.VideoCapture(self.input_path)
-        size = (int(movie.get(cv2.CAP_PROP_FRAME_WIDTH)), int(movie.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        size = (
+            int(movie.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(movie.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        )
         fps = movie.get(cv2.CAP_PROP_FPS)
-        sink = cv2.VideoWriter(self.output_path, cv2.CAP_ANY, cv2.VideoWriter_fourcc(*'MJPG'), fps, size, params=None)
+        sink = cv2.VideoWriter(
+            self.output_path,
+            cv2.CAP_ANY,
+            cv2.VideoWriter_fourcc(*"MJPG"),
+            fps,
+            size,
+            params=None,
+        )
 
         tasks = Queue()
         results = Queue()
@@ -110,7 +135,6 @@ class Master(Thread):
             workers.append(worker)
             worker.start()
 
-
         no_more_frames = False
         frame_idx = 0
         last_saved_idx = -1
@@ -119,14 +143,14 @@ class Master(Thread):
             ret, frame = movie.read()
             # no more frames
             if ret:
-                while tasks.qsize() >= 2*self.worker_count:
+                while tasks.qsize() >= 2 * self.worker_count:
                     sleep(0.01)
                 tasks.put((frame_idx, frame))
                 frame_idx += 1
             else:
                 no_more_frames = True
 
-            #Flush frames to dst file
+            # Flush frames to dst file
             if not results.empty():
                 tmp_res = []
                 print("FLUSHING FRAMES len(results)={}".format(results.qsize()))
@@ -138,7 +162,11 @@ class Master(Thread):
                         # there is no next frame processed yet
                         for fallback_res in tmp_res:
                             results.put(fallback_res)
-                        print("FLUSHING FRAMES BREAKING!! len(results)={}".format(results.qsize()))
+                        print(
+                            "FLUSHING FRAMES BREAKING!! len(results)={}".format(
+                                results.qsize()
+                            )
+                        )
                         break
                     sink.write(res[1][1])
                     print("==========WROTE {} frame".format(res[0]))
@@ -150,12 +178,3 @@ class Master(Thread):
                 sink.release()
                 for w in workers:
                     w.kill()
-
-
-
-
-
-
-
-
-
