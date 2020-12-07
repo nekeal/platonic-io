698cef21c92d5cb4cc2d2d57c1a71716e50211fa
Szymon Cader <szymon.sc.cader@gmail.com>
2020-12-07 19:33:54 +0100

    Reformat file

--- platonic_io/get_plate.py
+++ platonic_io/get_plate.py
@@ -1,23 +1,20 @@
+from os.path import splitext
+
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import gridspec
 
 from .local_utils import detect_lp
-from os.path import splitext, basename
-from keras.models import model_from_json
-import glob
-import time
-from sklearn.preprocessing import LabelEncoder
 
 
 def load_model(path):
+    from keras.models import model_from_json  # long running import
+
     try:
         path = splitext(path)[0]
-        with open('%s.json' % path, 'r') as json_file:
+        with open("%s.json" % path, "r") as json_file:
             model_json = json_file.read()
         model = model_from_json(model_json, custom_objects={})
-        model.load_weights('%s.h5' % path)
+        model.load_weights("%s.h5" % path)
         print("Loading model successfully...")
         return model
     except Exception as e:
@@ -58,6 +55,7 @@ def draw_box(image, cor, thickness=3):
         cv2.polylines(vehicle_image, [pts], True, (0, 255, 0), thickness)
     return vehicle_image
 
+
 def get_width_height_ratio(cor):
     x_coordinates = cor[0]
     y_coordinates = cor[1]
@@ -68,22 +66,27 @@ def get_width_height_ratio(cor):
         pts.append([int(x_coordinates[i]), int(y_coordinates[i])])
     width = abs(pts[1][0] - pts[0][0])
     height = abs(pts[0][1] - pts[2][1])
-    if height==0: height=0.001
+    if height == 0:
+        height = 0.001
     # width = sqrt((pts[1][0] - pts[0][0]) ** 2 + (pts[1][1] - pts[0][1]) ** 2)
     # height = sqrt((pts[0][0] - pts[2][0]) ** 2 + (pts[0][1] - pts[2][1]) ** 2)
-    return width/height
+    return width / height
 
 
 def sort_contours(cnts, reverse=False):
     i = 0
     boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
+    (cnts, boundingBoxes) = zip(
+        *sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse)
+    )
     return cnts
 
+
 # pre-processing input images and pedict with model
-def predict_from_model(image,model,labels):
-    image = cv2.resize(image,(80,80))
-    image = np.stack((image,)*3, axis=-1)
-    prediction = labels.inverse_transform([np.argmax(model.predict(image[np.newaxis,:]))])
+def predict_from_model(image, model, labels):
+    image = cv2.resize(image, (80, 80))
+    image = np.stack((image,) * 3, axis=-1)
+    prediction = labels.inverse_transform(
+        [np.argmax(model.predict(image[np.newaxis, :]))]
+    )
     return prediction
