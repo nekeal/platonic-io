48fcaec6b2fda7f20e221223cd1b2ff8f1bf1dc2
Szymon Cader <szymon.sc.cader@gmail.com>
2020-12-13 22:11:11 +0100

    Improve logger

--- platonic_io/__init__.py
+++ platonic_io/__init__.py
@@ -5,13 +5,21 @@ __version__ = "0.2.1"
 import logging
 import sys
 
-logger = logging.getLogger()
+logger = logging.getLogger("platonic_io")
+
+_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
 logger.setLevel(logging.DEBUG)
+
 _sh = logging.StreamHandler(stream=sys.stdout)
 _sh.setLevel(logging.DEBUG)
+_sh.setFormatter(_formatter)
 
 _fh = logging.FileHandler("platonic-main.log")
 _fh.setLevel(logging.DEBUG)
+_fh.setFormatter(_formatter)
 
 logger.addHandler(_sh)
 logger.addHandler(_fh)
